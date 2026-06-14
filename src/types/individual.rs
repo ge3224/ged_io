3f5a0d8ccd6df76e7385332538a4c049d8828f96
n4n5 <its.just.n4n5@gmail.com>
2026-06-14 23:48:23 +0200

    fmt

--- src/types/individual.rs
+++ src/types/individual.rs
@@ -206,7 +206,7 @@ impl Individual {
     #[must_use]
     pub fn full_name(&self) -> Option<String> {
         self.name.as_ref().and_then(|name| name.full_name())
-   }
+    }
 
     /// Gets the given (first) name if available.
     #[must_use]
