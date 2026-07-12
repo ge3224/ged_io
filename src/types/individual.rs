cca7b2bdc1fa30ccad5400e20c43dc8ec65dd836
n4n5 <its.just.n4n5@gmail.com>
2026-07-12 21:06:21 +0200

    add methode

--- src/types/individual.rs
+++ src/types/individual.rs
@@ -227,17 +227,13 @@ impl Individual {
     /// Checks if the individual is male.
     #[must_use]
     pub fn is_male(&self) -> bool {
-        self.sex
-            .as_ref()
-            .is_some_and(|s| matches!(s.value, GenderType::Male))
+        self.sex.as_ref().is_some_and(gender::Gender::is_male)
     }
 
     /// Checks if the individual is female.
     #[must_use]
     pub fn is_female(&self) -> bool {
-        self.sex
-            .as_ref()
-            .is_some_and(|s| matches!(s.value, GenderType::Female))
+        self.sex.as_ref().is_some_and(gender::Gender::is_female)
     }
 
     /// Gets the birth event details if available.
