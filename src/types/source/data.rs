db56eb5bab1f19a55158090918e19392fddfd57b
ge3224 <ge3224@gmail.com>
2026-07-25 16:15:36 -0500

    feat: migrate Source/Data substructure to Arena

--- src/types/source/data.rs
+++ src/types/source/data.rs
@@ -1,19 +1,19 @@
 #[cfg(feature = "json")]
 use serde::{Deserialize, Serialize};
 
-use crate::types::event::detail::Detail;
+use crate::{arena::Arena, types::event::detail::Detail};
 
 #[allow(clippy::module_name_repetitions)]
 #[derive(Debug, Default, PartialEq)]
 #[cfg_attr(feature = "json", derive(Serialize, Deserialize))]
 pub struct Data {
-    events: Vec<Detail>,
+    events: Arena<Detail>,
     pub agency: Option<String>,
 }
 
 impl Data {
     pub fn add_event(&mut self, event: Detail) {
-        self.events.push(event);
+        self.events.insert(event);
     }
 
     pub(crate) fn outbound_refs(&self, sink: &mut impl FnMut(&str)) {
